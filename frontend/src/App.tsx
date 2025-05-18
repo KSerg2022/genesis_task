11d4ff31a1736d1aaedbe00b70148b56ea7d59e0
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2025-05-18 11:28:35 +0200

    added tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 export const App = () => {
   return (
     <>
-      <p className="read-the-docs">
+      <p className="ext-3xl font-bold underline">
         Click on the Vite and React logos to learn more
       </p>
     </>
